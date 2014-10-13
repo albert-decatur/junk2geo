edd14e9d99b0452c4f109f6db776bc5b0ab2eee6
Albert Decatur <adecatur@aiddata.org>
2014-10-12 21:03:21 -0400

    reduce crs sample

--- junk2geo.sh
+++ junk2geo.sh
@@ -17,7 +17,7 @@ OPTIONS:
 
 Multiple values of ISO2 ought to be pipe separated in the second column of the input TSV (using the -i flag) like so: IR|US|CN.
 If no ISO2 is appropriate simply do not include any. 
-Example use: $0 -e 1 -g geonames/geonames_2014-10-02.sqlite -i input.tsv
+Example use: $0 -e 1 -g geonames/geonames_2014-10-02.sqlite -i test/crs2014-06-16_sample.tsv
 
 EOF
 }
@@ -42,3 +42,26 @@ do
 done
 
 	#tre-agrep -E $errors -w -e "$place" --show-position $to_geo |\
+# make apostraphe a variable for GNU parallel
+a="'"
+cat $to_geo |\
+mawk -F'\t' '{print $1}'|\
+sed '1d'|\
+sort|\
+uniq|\
+parallel -j 1 --gnu '
+	iso2s=$( echo {} | tr "|" "\n" )
+	# if ISO2 list is not blank (which signifies use all ISO2) then write the SQL to make a table with all of the allCountries table given those ISO2
+	# this strategy presumes that queries will be used many times each
+	if [[ -n $( echo "$iso2s" | grep -vE "^$" ) ]]; then 
+		sql=$( 
+			echo "$iso2s" |\
+			sed "s:^:'$a':g;s:^:countrycode = :g;s:$:'$a':g;s:$: OR:g" |\
+			tr "\n" " " |\
+			sed "s:OR $::g" |\
+			sed "s:^:DROP TABLE IF EXISTS \"{}\"\; CREATE TABLE \"{}\" AS SELECT geonameid,name,asciiname,alternatenames FROM allCountries WHERE :g;s:$:\;:g"
+		)
+	fi
+	echo "$sql"|\
+	sqlite3 '$geonames'
+'
